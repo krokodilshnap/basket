de3e8f982d3e1e48e5cfd569e3ea43a7e55c715c
Dmitriy <krokodilshnap@gmail.com>
2018-08-18 00:02:44 +0300

    Добавил остальные свойства, проба пера для уникальных объектов

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,17 @@
 function SmartBasket(options) {
-    var wrapper, art, picture, name, amount, mark, price, submit, currency, basket;
+    var wrapper, art, picture, name, amount, mark, price, submit, currency, basket, clearBtn;
     var data;
     var basketItemId = "smartBasketItem";
     var basket = document.querySelector(options.basket);
+    var clearBtn = document.querySelector(options.clearBtn);
 
     countBasket();
 
     document.onclick = function(event) {
-        if (event.target.closest(options.wrapper)) {
-            wrapper = event.target.closest(options.wrapper);
+        var target = event.target;
+
+        if (target.closest(options.wrapper)) {
+            wrapper = target.closest(options.wrapper);
             art = wrapper.querySelector(options.art);
             picture = wrapper.querySelector(options.pic);
             name = wrapper.querySelector(options.name);
@@ -18,16 +21,32 @@ function SmartBasket(options) {
             submit = wrapper.querySelector(options.submitBtn);
             currency = wrapper.querySelector(options.currency);
 
+
+
             data = {
-                art: art.textContent,
-                name: name.textContent
+                characters: {
+                    art: art.textContent,
+                    name: name.textContent,
+                    mark: getSelectedMark(mark),
+                    price: price.innerHTML
+                },
+                add: {
+                    picture: getPictureUrl(picture)
+                },
+                total: {
+                    amount: +amount.value
+                }
+
             };
-
-
 
         }
 
-        if (event.target != submit) return;
+        if (target == clearBtn) {
+            clearAllBasket(target);
+            countBasket();
+        }
+
+        if (target != submit) return;
 
         dataToBasket();
 
@@ -36,12 +55,40 @@ function SmartBasket(options) {
 
     }
 
+
+
+    // Получение картинки из background
+    function getPictureUrl(elem) {
+        var style = elem.style.backgroundImage;
+
+        return style.replace(/(url\(|\)|")/g, '');
+    }
+
+    //Очистка всей корзины
+    function clearAllBasket(button) {
+        for (var key in localStorage) {
+            if (key.indexOf(basketItemId) != -1) {
+                localStorage.removeItem(key);
+            }
+        }
+
+    }
+
+    // Получение марки из селекта
+    function getSelectedMark(elem) {
+        for (var i = 0; i < elem.options.length; i++) {
+            if (elem.options[i].selected) {
+                return elem.options[i].innerHTML;
+            }
+        }
+    }
+
+    // Добавление всех параметров в корзину
     function dataToBasket() {
         toBasket(data);
     }
 
     // Возвращает количество итемов в LocalStorage
-
     function localStorageCount() {
         var localStorageItems = 0;
 
@@ -56,22 +103,23 @@ function SmartBasket(options) {
     }
 
     //Подсчет количества итемов в корзине
-
     function countBasket() {
         basket.innerHTML = localStorageCount();
     }
 
     
     // Добавление объекта в LocalStorage
-
     function toBasket(object) {
-        console.log(object)
         var str = JSON.stringify(object);
+
         localStorage.setItem(basketItemId + ' - ' + Math.random(10000), str);
-        compareObjects();
+
+        getUniqueItems();
     }
 
-    function compareObjects() {
+    //Получаем только уникальные объекты из LocalStorage, плюсуем количество и записываем обратно
+
+    function getUniqueItems() {
         var buffer = {};
 
         for (var key in localStorage) {
@@ -81,14 +129,29 @@ function SmartBasket(options) {
             }
         }
 
-        for (var key in buffer) {
-            for (var item in data) {
-                console.log(buffer[key][item]);
+        for (var item in buffer) {
+            var firstElem = JSON.stringify(buffer[item].characters);
+
+            for (var subitem in buffer) {
+                if (subitem == item) continue;
+
+                var secondElem = JSON.stringify(buffer[subitem].characters);
+
+                if (firstElem == secondElem) {
+                    buffer[item].total.amount += buffer[subitem].total.amount;
+                    delete buffer[subitem];
+                }
+
             }
+
         }
 
 
-        console.log(buffer);
+        for (var k in buffer) {
+            var str = JSON.stringify(buffer[k]);
+
+            localStorage.setItem('' + k, str);
+        }
 
     }
 
